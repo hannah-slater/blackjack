af6f0e2fa58bf7ac6186efcd69cbc258ab28a6ba
Hannah Slater <hannahslater@MacBook-Pro.home>
2022-12-20 14:46:36 +0000

    Added in ace fluctuation calculations and seperated calculating value and returning value

--- src/hand.py
+++ src/hand.py
@@ -4,21 +4,44 @@ class Hand:
     def __init__(self):
         self.cards = []
 
+    #adds a card object to the hand
     def addCard(self, card):
         self.cards.append(card)
+        self.calculateValue()
 
-    def getCards(self):
-        return self.cards
-
+    #returns a string containg the cards present in the hand
     def displayCards(self):
         string = ""
         for card in self.cards:
-            string += card.getCard()
+            string += card.getCard() + "\n"
         return string
 
-    def getValue(self):
+    #calculates the value of the hand
+    def calculateValue(self):
         value = 0
+        #holds the value of the amount of aces in the hand
+        ace = 0
+        
         for card in self.cards:
-            a = card.getValue()
-            value += a
-        return value
+            v = card.getValue()
+            if v == 1:
+                ace += 1
+            else:
+                value += v
+        
+        #evaluates the value of aces if there are one or more in the hand
+        if ace != 0:
+            while ace >= 2:
+                value += 1
+                ace -= 1
+
+            if ace == 1:
+                if (value + 11) <= 21:
+                    value += 11
+                else:
+                    value += 1
+        self.handValue = value
+
+    #returns the value of the hand
+    def getValue(self):
+        return self.handValue
