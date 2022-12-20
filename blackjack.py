f7c9dca825695c33ed0b729eef708ec4ddf59104
Hannah Slater <hannahslater@MacBook-Pro.home>
2022-12-20 14:02:12 +0000

    functionality of game added

--- blackjack.py
+++ blackjack.py
@@ -1,33 +1,59 @@
 from src.deck import Deck
 from src.card import Card
+from src.hand import Hand
 
 def play():
     deck = Deck()
     #deck.shuffle()
 
-    #Player draw:
-    a = deck.draw()
-    b = deck.draw()
-    print(calcValue(a,b))
+    #Hands
+    playerHand = Hand()
+    dealerHand = Hand()
 
     #Dealer draw:
-    c = deck.draw()
-    d = deck.draw()
-    print(calcValue(c,d))
+    dealerHand.addCard(deck.draw())
+    dealerHand.addCard(deck.draw())
+    dealerValue = dealerHand.getValue()
+    print(dealerHand.displayCards())
+    print("Value of dealer hand:",dealerValue)
+
+    #Player draw:
+    playerHand.addCard(deck.draw())
+    playerHand.addCard(deck.draw())
+    playerValue = playerHand.getValue()
+    print(playerHand.displayCards())
+    print("Value of Hand:",playerValue)
     
     #Player given options 'Hit' or 'Stick' (buttons)
-    #if Hit player recieves another card and value is updated
+    while playerValue < 21:
+        i = input("Hit or Stand?\n")
+        #if Hit player recieves another card and value is updated
+        if i.lower() == "hit":
+            playerHand.addCard(deck.draw())
+            playerValue = playerHand.getValue()
+            print(playerHand.displayCards())
+            print("Value of Hand:",playerValue)
+        elif i.lower() == "stand":
+            break
 
-    #if stick, dealer plays by house rules
 
-    #Win/Loss decided
-    #play next round?
+    #dealer turn
+    if playerValue <= 21:
+        while dealerValue < playerValue:
+            dealerHand.addCard(deck.draw())
+            dealerValue = dealerHand.getValue()
+            print(dealerHand.displayCards())
+            print("Value of dealer hand:",dealerValue)
+        if playerValue > dealerValue or dealerValue > 21:
+            print("WIN")
+        elif playerValue == dealerValue:
+            print("DRAW")
+        elif dealerValue > playerValue:
+            print("LOSE")
+    else:
+        print("BUST")
 
-def calcValue(c1, c2):
-    #add in ace rules
-    
-    value = c1.getValue() + c2.getValue()
-    return value
+
 
 if __name__ == '__main__':
     play()
